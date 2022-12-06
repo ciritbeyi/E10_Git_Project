daffa43245f3c61623cfffdcadaa01e1f91ee397
Ozgur Ciritbeyi <ozgrcrtby@gmail.com>
2022-12-06 20:45:34 +0100

    update First File

--- src/day0/First.java
+++ src/day0/First.java
@@ -3,5 +3,10 @@ package day0;
 public class First {
     public static void main(String[] args) {
         System.out.println("This is the first class");
+
+        // let's make this updates
+              
+            System.out.println("try again");
+
     }
 }
