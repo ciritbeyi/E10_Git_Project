3fcfc8a5579651e9210c764368899b47faff54d4
Ozgur Ciritbeyi <ozgrcrtby@gmail.com>
2022-12-06 20:49:16 +0100

    Wrote the code for First class

--- src/day0/First.java
+++ src/day0/First.java
@@ -5,8 +5,15 @@ public class First {
         System.out.println("This is the first class");
 
         // let's make this updates
-              
+
             System.out.println("try again");
 
+        sum(5,10);
+
+    }
+
+    public static void sum(int a,int b){
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
